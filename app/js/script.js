8568ade95c041606ab7fb7397558acc43d70f076
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2021-12-08 01:47:23 +0000

    Overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
-btnHamburger.addEventListener('click', function() {
+btnHamburger.addEventListener('click', function() {  // Close Hamburger Menu
     console.log('click hamburger');
 
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
-    else {
-        btnHamburger.classList.add('open');
+    else { // Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
